91ba4f60348d9480b45d138e1b04923aa7c8d474
Joe Ng <joe_ng@Joe-MacBook-Pro.local>
2022-10-23 01:11:08 -0600

    Import from Spring Boot Initializr

---
+++ src/main/java/com/livebarn/sushishop/SushishopApplication.java
@@ -0,0 +1,13 @@
+package com.livebarn.sushishop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SushishopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SushishopApplication.class, args);
+	}
+
+}
